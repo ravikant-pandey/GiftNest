f44b4fa8fe066fc2017255d1d6365089a78b3a7f
Ravikant Pandey <howtoearn554@gmail.com>
2026-02-14 21:21:58 +0530

    Fix import paths for Home, About, and Contact components in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 import Collection from "./Pages/Collection";
 import Orders from "./Pages/Orders";
 import Login from "./Pages/Login";
