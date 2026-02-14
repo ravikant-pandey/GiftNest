7be4e286b345b0e213e2234cf9a0d1e7be60023e
Ravikant Pandey <howtoearn554@gmail.com>
2026-02-14 22:58:06 +0530

    Fix casing in import path for AppContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AppContextProvider from "./context/AppContext.jsx";
+import AppContextProvider from "./Context/AppContext.jsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
